ea421415695cd0477d2bcb3409897e15fbd601b1
vdionysio <vini.dionysio@gmail.com>
2022-07-07 13:21:16 -0300

    chore: set initial structure

---
+++ src/main/java/br/com/ecoveco/datamanager/DatamanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecoveco.datamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamanagerApplication.class, args);
+	}
+
+}
